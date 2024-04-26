7e178a5e71497837e4110378c409109e77b13dbf
sambitxdev <kumarsambit000@gmail.com>
2024-04-26 17:13:31 +0530

    changes

--- src/components/footer.js
+++ src/components/footer.js
@@ -77,7 +77,7 @@ const Footer = () => {
     if (process.env.NODE_ENV !== 'production') {
       return;
     }
-    fetch('')
+    fetch('https://api.github.com/repos/bchiang7/v4')
       .then(response => response.json())
       .then(json => {
         const { stargazers_count, forks_count } = json;
@@ -106,6 +106,7 @@ const Footer = () => {
 
       <StyledCredit tabindex="-1">
         <a href="https://v4.brittanychiang.com/">
+          <div>Inspired by Brittany Chiang</div>
           {githubInfo.stars && githubInfo.forks && (
             <div className="github-stats">
               <span>
